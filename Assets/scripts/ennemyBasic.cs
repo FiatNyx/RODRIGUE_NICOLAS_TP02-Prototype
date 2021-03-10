327aa1325f123d8906bfe1975e727af97e0d583e
Nicolas Rodrigue <nyx.rodrigue@yahoo.ca>
2021-03-09 23:00:47 -0500

    Mouvement de base
    Création du navmesh, du script de base du mouvement du joueur et d'un enmemi

---
+++ Assets/scripts/ennemyBasic.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ennemyBasic : MonoBehaviour
+{
+	NavMeshAgent navMeshAgent;
+	bool isMoving = false;
+	float timerMove = 0f;
+	public GameObject player;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+	private void Awake()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		timerMove = 0f;
+		StartCoroutine(Mouvement());
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+
+	IEnumerator Mouvement()
+	{
+		Debug.Log("Se déplace");
+		isMoving = true;
+
+		navMeshAgent.speed = 3;
+
+		//Me déplacer vers la destination
+		navMeshAgent.SetDestination(player.transform.position);
+		
+
+		//Tant que je ne suis pas rendu à destination, je ne fait rien d'autre
+		while (navMeshAgent.pathPending || (navMeshAgent.remainingDistance > 0.5f && timerMove < 3f))
+		{
+			Debug.Log("Salut");
+			Debug.Log(timerMove);
+			timerMove += Time.deltaTime;
+			yield return null;
+		}
+
+		print("Finis");
+		//Rendu à destination, je prend une pause (Bien méritée)
+		if (navMeshAgent.remainingDistance > 0.5f)
+		{
+			navMeshAgent.isStopped = true;
+		}
+
+		//Je démarre une nouvelle patrouille
+		isMoving = false;
+	}
+}
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class player : MonoBehaviour
+{
+	Camera mainCam;
+	NavMeshAgent navMeshAgent;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		mainCam = Camera.main;
+
+	}
+
+	private void Awake()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+		{
+			//Créer un rayon entre la caméra et le curseur.
+			Ray camRay = mainCam.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit hit;
+			//Y a-t-il eu un impact
+			if (Physics.Raycast(camRay, out hit))
+			{
+				//Demander au pnj de s'y rendre
+				navMeshAgent.SetDestination(hit.point);
+			}
+
+
+
+		}
+	}
+}
